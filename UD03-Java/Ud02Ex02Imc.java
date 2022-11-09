ef4560f0f3b3ef6dcb5a01c6c240cad962d03f10
aguadodev <aguado@edu.xunta.es>
2022-11-09 13:10:59 +0100

    Ud02Ex02Imc.java

--- UD03-Java/Ud02Ex02Imc.java
+++ UD03-Java/Ud02Ex02Imc.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Ud02Ex02Imc {
     public static void main(String[] args) {
-        Scanner sc;
-        sc = new Scanner(System.in);
-        System.out.print("Introduce la altura en cm:");
-        int altura = sc.nextInt();
-        System.out.print("Introduce el peso en kilos:");
-        double peso = sc.nextDouble();
 
-        double imc = imc(peso, altura);
+        int altura = leerAltura(50, 250);
 
-        System.out.println("El IMC es" + imc);
+        double peso = leerPeso(20, 300);
+
+        imprimirImc(imc(peso, altura));        
+    }
+
+
+    public static void imprimirImc(double imc){
+        System.out.printf("El IMC es %.2f. ", imc);
+
         if (imc < 18.5) {
             System.out.println("Bajo Peso");
         } else if(imc < 25){
@@ -21,8 +23,51 @@ public class Ud02Ex02Imc {
         } else {
             System.out.println("Obesidad");
         }
-        
     }
+
+    public static double leerPeso(double minKg, double maxKg) {
+        Scanner sc;
+        double peso = 0;                      
+    
+        boolean entradaValida = false;
+        while (!entradaValida){
+            sc = new Scanner(System.in);
+            try {
+                System.out.print("Introduce el peso en kilos: ");
+                peso = sc.nextDouble();
+                if (peso >= minKg && peso <= maxKg)
+                    entradaValida = true;
+                else
+                    System.out.println("El peso debe estar entre " + minKg + " y " + maxKg + " cm.");
+            } catch (Exception e){
+                System.out.println("Error de entrada de datos. Por favor introduce números. Usa la coma como separador decimal.");
+            }            
+        }
+        return peso;
+    }
+
+    public static int leerAltura(int minCm, int maxCm) {
+        Scanner sc;
+        int altura = 0;                      
+
+        boolean entradaValida = false;
+        while (!entradaValida){
+            sc = new Scanner(System.in);
+            try {                
+                System.out.print("Introduce la altura en cm: ");
+                altura = sc.nextInt();
+                if (altura >= minCm && altura <= maxCm) {
+                    entradaValida = true;
+                } else {
+                    System.out.println("La altura debe estar entre "+ minCm +" y "+ maxCm +" cm.");
+                }
+            } catch (Exception e){
+                System.out.println("Error de entrada de datos. Por favor introduce números enteros.");
+            }
+        }        
+        return altura;
+    }
+
 
     public static double imc(double kg, int cm) {
         return kg / Math.pow(cm / 100.0, 2);
