40023d8feb5d88fb28f4dd6778e52cca01b45968
aguadodev <aguado@edu.xunta.es>
2022-12-05 12:31:33 +0100

    Arrays nombres

--- UD04/Arrays/src/App.java
+++ UD04/Arrays/src/App.java
@@ -1,34 +1,25 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("ARRAYS!!!");
+        // Declaración constante tamaño arrays
+        final int NUM_ALUMNOS = 30;
+        
+        // Declaración array de nombres inicializado
+        String nombres[] = {"Alex","Xoán","Patricia","Brais","Ander","Javier","Fabricio","Hugo F.","Manuel","David G.","Pilar","María","Ramón","Xoel","Hugo I.","Andrés","César","David M.","Borja","Xabier","Diego","Girma","Yago","Anxo","David S.","Mario","Daniel","Antonella","Manuel"};
 
-        final int NUM_ALUMNOS = 3000;
-
+        // Declaración de variables de arrays
         double notasReales[];   // 8
         int notasBoletin[];     // 4
-        boolean aprobados[];    // 1b
-        String nombres[];       // Referencia
-        
-        int notas2Av[], notas3Av[] = null;
+        boolean aprobados[];    // 1b                    
 
+        // Creamos los arrays con new
         notasReales = new double[NUM_ALUMNOS];
         notasBoletin = new int[NUM_ALUMNOS];
         aprobados = new boolean[NUM_ALUMNOS];
-        nombres = new String[NUM_ALUMNOS];
-        notas2Av = notasBoletin;
 
-        System.out.println(notasReales);
-        System.out.println(notasBoletin);        
-        System.out.println(notas2Av);        
-        System.out.println(notas3Av);        
-        System.out.println(aprobados);
-        System.out.println(nombres);
-
-        notasBoletin[5] = 8;
-        notas2Av[5]++;
-
-        System.out.println(notas2Av[5]);  
-        System.out.println(nombres[5]);  
-     
+        // Inicializar notasReales a valores aleatorios flotantes entre 0 y 10;
+        for(int i = 0; i < NUM_ALUMNOS; i++){
+            notasReales[i] = // Valora aleatorio entre 0 y 10 inclusives
+        }     
     }
 }
