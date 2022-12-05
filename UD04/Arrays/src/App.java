c54cfd55f0bdd02d7481de05f7eb3d3971fbe609
aguadodev <aguado@edu.xunta.es>
2022-12-05 13:45:28 +0100

    arrays

--- UD04/Arrays/src/App.java
+++ UD04/Arrays/src/App.java
@@ -25,10 +25,10 @@ public class App {
 
         // Inicializar notasReales a valores aleatorios flotantes entre 0 y 10;
         for(int i = 0; i < NUM_ALUMNOS; i++){
-            notasReales[i] = Math.random() * 10; // Valora aleatorio entre 0 y 10 inclusives
+            notasReales[i] = Math.random() * 8 + 2; // Valora aleatorio entre 0 y 10 inclusives
         }  
 
-        // Inicializar notasBoletion a valores redondeados de notasReales. El valor mínimo es 1.
+        // Inicializar notasBoletin a valores redondeados de notasReales. El valor mínimo es 1.
         for(int i = 0; i < NUM_ALUMNOS; i++){
             notasBoletin[i] = (int) Math.round(notasReales[i]);
             if (notasBoletin[i] == 0)
@@ -40,6 +40,51 @@ public class App {
         for(int i = 0; i < NUM_ALUMNOS; i++){
             System.out.printf("%10s %10.2f %10d", nombres[i], notasReales[i], notasBoletin[i]);
             System.out.println("\t" + aprobados[i]);
+        }    
+        
+        System.out.printf("La nota media de la clase es de %.2f %n", media(notasReales));
+        System.out.printf("La nota media de la clase en el boletín es de %.2f %n", media(notasBoletin));
+
+        // Aprobado general con las notas del boletín
+        int[] notasAprobadoGeneral = aprobadoGeneral(notasBoletin);
+
+        // Vuelve a imprimir las tablas de notas
+        System.out.println("    Alumn@   NotaReal    Boletin     BoletinAprobados Aprobado");
+        for(int i = 0; i < NUM_ALUMNOS; i++){
+            System.out.printf("%10s %10.2f %10d %10d", nombres[i], notasReales[i], notasBoletin[i], notasAprobadoGeneral[i]);
+            System.out.println("\t" + aprobados[i]);
         }
     }
+
+    static int[] aprobadoGeneral(int[] t){
+        // Devuelve una copia del array original con los elementos cuyo valor es menor que 5 igual a 5
+        int[] tCopia = new int[t.length];
+        for(int i = 0; i < t.length; i++)
+            if (t[i] < 5)
+                tCopia[i] = 5;
+            else
+                tCopia[i] = t[i];
+        return tCopia;
+    }
+
+    static double media(int[] t){
+        // Devuelve la media de los elementos del array
+        double suma = 0;
+
+        for(int i = 0; i < t.length; i++){
+            suma += t[i];
+        }
+        return suma / t.length;
+    }    
+
+    static double media(double[] t){
+        // Devuelve la media de los elementos del array
+        double suma = 0;
+
+        for(int i = 0; i < t.length; i++){
+            suma += t[i];
+        }
+        return suma / t.length;
+    }
+
 }
