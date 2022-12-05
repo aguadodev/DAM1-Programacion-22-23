c3cfad72529ab12a527a7a24686022445b852b1b
aguadodev <aguado@edu.xunta.es>
2022-12-05 12:52:30 +0100

    Arrays: notas

--- UD04/Arrays/src/App.java
+++ UD04/Arrays/src/App.java
@@ -1,11 +1,17 @@
 public class App {
     public static void main(String[] args){
         System.out.println("ARRAYS!!!");
-        // Declaración constante tamaño arrays
-        final int NUM_ALUMNOS = 30;
         
         // Declaración array de nombres inicializado
-        String nombres[] = {"Alex","Xoán","Patricia","Brais","Ander","Javier","Fabricio","Hugo F.","Manuel","David G.","Pilar","María","Ramón","Xoel","Hugo I.","Andrés","César","David M.","Borja","Xabier","Diego","Girma","Yago","Anxo","David S.","Mario","Daniel","Antonella","Manuel"};
+        String nombres[] = {"Alex","Xoán","Patricia","Brais","Ander",
+                            "Javier","Fabricio","Hugo F.","Manuel","David G.",
+                            "Pilar","María","Ramón","Xoel","Hugo I.",
+                            "Andrés","César","David M.","Borja","Xabier",
+                            "Diego","Girma","Yago","Anxo","David S.",
+                            "Mario","Daniel","Antonella","Manuel"};
+
+        // Declaración constante tamaño arrays
+        final int NUM_ALUMNOS = nombres.length;
 
         // Declaración de variables de arrays
         double notasReales[];   // 8
@@ -19,7 +25,13 @@ public class App {
 
         // Inicializar notasReales a valores aleatorios flotantes entre 0 y 10;
         for(int i = 0; i < NUM_ALUMNOS; i++){
-            notasReales[i] = // Valora aleatorio entre 0 y 10 inclusives
-        }     
+            notasReales[i] = Math.random() * 10; // Valora aleatorio entre 0 y 10 inclusives
+        }  
+
+        // 
+        
+        for(int i = 0; i < NUM_ALUMNOS; i++){
+            System.out.printf("La nota de %s es %.2f %n", nombres[i], notasReales[i]);
+        }
     }
 }
