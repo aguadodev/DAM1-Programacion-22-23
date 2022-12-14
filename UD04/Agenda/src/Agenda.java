ced70b74eca784c48bc0f09bc69435c7f5162b61
aguadodev <aguado@edu.xunta.es>
2022-12-14 14:15:17 +0100

    Agenda - añadir y listar

--- UD04/Agenda/src/Agenda.java
+++ UD04/Agenda/src/Agenda.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Agenda {
-    Persona[] contactos;
+    static Persona[] contactos = new Persona[0];
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
@@ -10,6 +11,13 @@ public class Agenda {
             // Leer opción
             opcion = sc.nextInt();
             // Ejecutar opción
+            switch(opcion){
+                case 1: anhadirContacto(); break;
+                case 2: listarContactos(); break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+            }
 
         } while( opcion != 0);        
     }
@@ -23,6 +31,28 @@ public class Agenda {
         System.out.println("4. Vaciar agenda");
         System.out.println("5. Buscar contacto");
         System.out.println("0. SALIR");    
+    }
+
+
+    static public void anhadirContacto(){
+        Scanner sc = new Scanner(System.in);
+        Persona p = new Persona();
+
+        System.out.print("Nombre: ");
+        p.setNombre(sc.nextLine());
+        System.out.print("Apellidos: ");
+        p.setApellidos(sc.nextLine());
+        System.out.print("Teléfono: ");
+        p.setTelefono(sc.nextLine());
+        System.out.print("Correo electrónico: ");
+        p.setCorreoE(sc.nextLine());
+
+        contactos = Arrays.copyOf(contactos, contactos.length + 1);
+        contactos[contactos.length - 1] = p;
+    }
+
+    static public void listarContactos(){
+        System.out.println(Arrays.toString(contactos));
     }
     
 }
