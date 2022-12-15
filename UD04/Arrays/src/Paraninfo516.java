5236063d97950f54bf9d8403a925954ffb7ad3d1
aguadodev <aguado@edu.xunta.es>
2022-12-15 14:13:14 +0100

    Recursividad

--- UD04/Arrays/src/Paraninfo516.java
+++ UD04/Arrays/src/Paraninfo516.java
@@ -1,5 +1,9 @@
 public class Paraninfo516 {
     public static void main(String[] args) {
+        /*
+         * PENDIENTE DE RESOLVER CUANDO DOS CASILLAS ESTÁN COMUNICADAS ENTRE SÍ 
+         * Y SE GENERA UN BUCLE EN LA PILA DE LLMADAS RECURSIVAS 
+         */
         int mapa[][] = {
             {0, 1, 0, 0, 0},
             {0, 0, 1, 1, 1},
@@ -8,10 +12,18 @@ public class Paraninfo516 {
             {0, 0, 0, 0, 0},
         };
 
-        System.out.println(hayCamino(mapa, 2, 4));
+        int mapa2[][] = {
+        //   0  1  2  3  4  5
+            {0, 1, 1, 0, 0, 0}, //0
+            {0, 0, 0, 1, 0, 0}, //1
+            {0, 0, 0, 0, 1, 0}, //2
+            {0, 1, 0, 0, 0, 0}, //3
+            {0, 0, 0, 0, 0, 1}, //4
+            {0, 0, 0, 0, 0, 0}, //5
+        };        
 
+        System.out.println(hayCamino(mapa2, 0, 5));
     }
-
     
     static boolean hayCaminoDirecto(int[][] t, int o, int d){          
         return t[o][d] == 1;
@@ -25,10 +37,9 @@ public class Paraninfo516 {
         else {
             for(int i = 0; i < t.length; i++){
                 if(hayCaminoDirecto(t, o, i))
-                    return hayCamino(t, i, d);
+                    hayCamino = hayCamino(t, i, d);
             }
-        }
-               
+        }               
 
         return hayCamino;
     }
