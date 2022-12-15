ca82269d970f6c216916177882de645afd649268
Óscar <aguado@edu.xunta.es>
2022-12-16 00:25:45 +0100

    Paraninfo516

--- Codigo variado/Java/JSON/src/ReadJsonFromUrl.java
+++ Codigo variado/Java/JSON/src/ReadJsonFromUrl.java
@@ -7,6 +7,7 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 import org.json.JSONArray;
@@ -34,7 +35,7 @@ public class ReadJsonFromUrl {
         }
     }    
 
-    public static String leerUrl
+
 
     public static void ejemploOpenFoodFacts(){
         try {
@@ -98,7 +99,7 @@ public class ReadJsonFromUrl {
                 + direccion.replace(" ", "+");
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws MalformedURLException{
         String urlConsola; 
 
         Scanner sc = new Scanner(System.in);
--- UD04/Arrays/src/Paraninfo516.java
+++ UD04/Arrays/src/Paraninfo516.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Paraninfo516 {
     public static void main(String[] args) {
         /*
-         * PENDIENTE DE RESOLVER CUANDO DOS CASILLAS ESTÁN COMUNICADAS ENTRE SÍ 
-         * Y SE GENERA UN BUCLE EN LA PILA DE LLMADAS RECURSIVAS 
+         * PROBLEMA DE GRAFOS
+         * 
+         * SOLUCIÓN A: Añadir una estructura que recuerde los lugares visitados y que
+         * se irá actualizando a lo largo de toda la pila de llamadas.
+         * Se pasa de una llamada a otra como un 4º parámetro.
+         * Se puede eliminar este 4º parámetro cun una versión sobrecargada del método para
+         * la primera llamada.
          */
         int mapa[][] = {
             {0, 1, 0, 0, 0},
@@ -21,27 +28,132 @@ public class Paraninfo516 {
             {0, 0, 0, 0, 0, 1}, //4
             {0, 0, 0, 0, 0, 0}, //5
         };        
+        /*
+         * 0 > 1 <> 3
+         *   > 2 > 4 > 5
+         */
 
-        System.out.println(hayCamino(mapa2, 0, 5));
+        // Pruebas con 3 implementaciones distintas
+        System.out.println(hayCamino(mapa2, 0, 5) + " " + 
+                           hayCaminoAnchura(mapa2, 0, 5) + " " + 
+                           hayCaminoProfundidad(mapa2, 0, 5));
+
+        System.out.println(hayCamino(mapa2, 1, 5) + " " + 
+                           hayCaminoAnchura(mapa2, 1, 5) + " " + 
+                           hayCaminoProfundidad(mapa2, 1, 5));
+
+                           
     }
     
+
+
     static boolean hayCaminoDirecto(int[][] t, int o, int d){          
         return t[o][d] == 1;
     }
 
-    static boolean hayCamino(int[][] t, int o, int d){
-        boolean hayCamino = false;
 
-        if (hayCaminoDirecto(t, o, d))
+    // Función sobrecargada para evitar el 4º parámetro;
+    static boolean hayCamino(int[][] t, int o, int d){
+        return hayCamino(t, o, d, new boolean[t.length]);
+    }
+
+    // Solución que implementa la búsqueda en profundidad con recursividad
+    static boolean hayCamino(int[][] t, int o, int d, boolean[] visitados){
+        boolean hayCamino = false;
+        visitados[o] = true; // Cada vez que visitamos un lugar ponemos a true su índice
+
+        if (o == d || hayCaminoDirecto(t, o, d))
             hayCamino = true;
         else {
             for(int i = 0; i < t.length; i++){
-                if(hayCaminoDirecto(t, o, i))
-                    hayCamino = hayCamino(t, i, d);
+                if(hayCaminoDirecto(t, o, i) && !visitados[i])
+                    hayCamino = hayCamino(t, i, d, visitados);
             }
         }               
-
         return hayCamino;
+    }
+
+
+    // Solución que implementa la búsqueda en profundidad con una pila
+    static boolean hayCaminoProfundidad(int[][] t, int o, int d){
+        // Crear una pila para almacenar los lugares a visitar
+        Stack<Integer> pila = new Stack<>();
+
+        // Crear un conjunto para almacenar los lugares visitados
+        Set<Integer> visitados = new HashSet<>();
+
+        // Añadir el lugar de origen a la pila
+        pila.push(o);
+
+        // Mientras la pila no esté vacía...
+        while (!pila.isEmpty()) {
+            // Sacar el primer lugar de la pila
+            int lugar = pila.pop();
+
+            // Si el lugar es el lugar de destino, entonces se ha encontrado un camino
+            // desde el lugar de origen al lugar de destino
+            if (lugar == d) {
+                return true;
+            }
+
+            // Marcar el lugar como visitado
+            visitados.add(lugar);
+
+            // Recorrer cada lugar conectado al lugar actual
+            for (int i = 0; i < t[lugar].length; i++) {
+                // Si el lugar está conectado y no ha sido visitado...
+                if (t[lugar][i] == 1 && !visitados.contains(i)) {
+                    // Añadir el lugar a la pila
+                    pila.push(i);
+                }
+            }
+        }
+
+        // Si se recorrió todo el grafo y no se encontró un camino, entonces no es
+        // posible viajar desde el lugar de origen al lugar de destino
+        return false;
+        }
+
+    
+
+    // Solución que implementa la búsqueda en anchura con una cola
+    static boolean hayCaminoAnchura(int[][] t, int o, int d){
+        // Crear una cola para almacenar los lugares a visitar
+        Queue<Integer> cola = new LinkedList<>();
+
+        // Crear un conjunto para almacenar los lugares visitados
+        Set<Integer> visitados = new HashSet<>();
+
+        // Añadir el lugar de origen a la cola
+        cola.add(o);
+
+        // Mientras la cola no esté vacía...
+        while (!cola.isEmpty()) {
+            // Sacar el primer lugar de la cola
+            int lugar = cola.remove();
+
+            // Si el lugar es el lugar de destino, entonces se ha encontrado un camino
+            // desde el lugar de origen al lugar de destino
+            if (lugar == d) {
+                return true;
+            }
+
+            // Marcar el lugar como visitado
+            visitados.add(lugar);
+
+            // Recorrer cada lugar conectado al lugar actual
+            for (int i = 0; i < t[lugar].length; i++) {
+                // Si el lugar está conectado y no ha sido visitado...
+                if (t[lugar][i] == 1 && !visitados.contains(i)) {
+                    // Añadir el lugar a la cola
+                    cola.add(i);
+                }
+            }
+        }
+
+        // Si se recorrió todo el grafo y no se encontró un camino, entonces no es
+        // posible viajar desde el lugar de origen al lugar de destino
+        return false;
     }
 
 }
