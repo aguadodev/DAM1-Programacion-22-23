cd56a67d326c21345335bfff25f784a7ab350176
aguadodev <aguado@edu.xunta.es>
2022-12-19 13:35:32 +0100

    Agenda

--- UD04/Agenda/src/Agenda.java
+++ UD04/Agenda/src/Agenda.java
@@ -1,9 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import javax.swing.text.html.parser.Entity;
+
+import com.google.gson.Gson;
 
 public class Agenda {
     static Persona[] contactos = new Persona[0];
     public static void main(String[] args) throws Exception {
+
+        // Importa la agenda de disco si existe.
+        importarAgenda();
+
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
         do{
@@ -15,8 +27,11 @@ public class Agenda {
                 case 1: anhadirContacto(); break;
                 case 2: listarContactos(); break;
                 case 3: eliminarContacto(); break;
-                case 4: break;
-                case 5: break;
+                case 4: vaciarAgenda(); break;
+                case 5: buscarContacto(); break;
+                case 6: importarAgenda(); break;
+                case 7: exportarAgenda(); break;
+                case 0: exportarAgenda(); break;
             }
 
         } while( opcion != 0);        
@@ -30,6 +45,8 @@ public class Agenda {
         System.out.println("3. Eliminar contacto");
         System.out.println("4. Vaciar agenda");
         System.out.println("5. Buscar contacto");
+        System.out.println("6. Importar");
+        System.out.println("7. Exportar");
         System.out.println("0. SALIR");    
     }
 
@@ -79,6 +96,107 @@ public class Agenda {
             }
             
         }
+
+        static void vaciarAgenda(){
+            contactos = new Persona[0];
+        }
+
+
+        static void buscarContacto(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nombre a buscar: ");
+            String nombre = sc.nextLine();
+
+            boolean encontrado = false;
+            
+            for(int i = 0; i < contactos.length; i++){
+                if(nombre.equalsIgnoreCase(contactos[i].getNombre())){
+                    System.out.println(contactos[i]);
+                    encontrado = true;
+                }
+                    
+            }
+
+            if(!encontrado)
+                System.out.println("No existe en la agenda nadie con el nombre de \"" + nombre + "\".");
+        }
+
+
+        static void exportarAgenda(){
+            // 1. Generar String JSON a partir del contenido de la agenda.
+            Gson gson = new Gson();
+            String json = gson.toJson(contactos);
+
+            System.out.println(json);
+            // 2. Escribe el string en un fichero de texto
+            writeStringToFile(json, "agenda.json");
+        }
+
+        static void importarAgenda(){
+            // 1. Leer el fichero de texto agenda.json a un String
+            String json = readFileToString("agenda.json");
+
+            // 2. Cargar el contenido del String/Json en el array de personas.
+            Gson gson = new Gson();
+            contactos = gson.fromJson(json, Persona[].class);
+        }
+
+
+
+    /**
+     * Crea un fichero de texto con el contenido de un String (fuente OpenAI)
+     * @param str
+     * @param filePath
+     */
+    public static void writeStringToFile(String str, String filePath) {
+        try {
+          // Creamos un objeto FileWriter que nos permitirá escribir en el fichero
+          FileWriter writer = new FileWriter(filePath);
+    
+          // Escribimos el String en el fichero
+          writer.write(str);
+    
+          // Cerramos el fichero
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      
+    /**
+     * Lee y carga el contenido de un fichero de texto a un String (fuente OpenAI)
+     * @param filePath
+     * @return
+     */
+    public static String readFileToString(String filePath) {
+        StringBuilder fileContent = new StringBuilder();
+        try {
+            // Creamos un objeto FileReader que nos permitirá leer el fichero
+            FileReader reader = new FileReader(filePath);
+        
+            // Creamos un buffer para leer el fichero de forma más eficiente
+            BufferedReader buffer = new BufferedReader(reader);
+        
+            // Leemos el fichero línea a línea
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                // Vamos añadiendo cada línea al StringBuilder
+                fileContent.append(line);
+                // Añadimos un salto de línea al final de cada línea
+                fileContent.append("\n");
+            }
+        
+            // Cerramos el buffer y el fichero
+            buffer.close();
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("No existe el fichero.");
+            //e.printStackTrace();
+        }
+    
+        // Devolvemos el contenido del fichero como un String
+        return fileContent.toString();
+    }
 
     }
 
