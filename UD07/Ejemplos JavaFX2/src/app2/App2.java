c39c7fd708f947e0b165850636dd95f4cb6f671b
aguadodev <aguado@edu.xunta.es>
2023-03-09 13:30:22 +0100

    Ejemplos JavaFX

---
+++ UD07/Ejemplos JavaFX2/src/app2/App2.java
@@ -0,0 +1,26 @@
+package app2;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App2 extends Application {
+
+    @Override public void start(Stage stage) {
+        Text text = new Text(10, 40, "Hello World!");
+        text.setFont(new Font(40));
+        Scene scene = new Scene(new Group(text));
+
+        stage.setTitle("Welcome to JavaFX!");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
