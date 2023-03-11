1c5c1e1623e5f3c7108b160b7db9c5abbf5d6561
Óscar <aguado@edu.xunta.es>
2023-03-11 20:49:29 +0100

    JavaFX - Memory

--- Codigo variado/Java/memory/src/App.java
+++ Codigo variado/Java/memory/src/App.java
@@ -1,106 +1,162 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
-        private static Scene scene;
-        static final int FILAS = 4;
-        static final int COLS = 4;
-    
-                // Array de JLabel para pintar las casillas en el mapa
-        static ImageView[][] mapa = new ImageView[FILAS][COLS];
-        // Dimensiones de alto y ancho en píxeles de las casillas
-        final int ALTO_CASILLA = 100;
-        final int ANCHO_CASILLA = 100;       
-    
-        @Override
-        public void start(Stage stage) throws IOException {
+    static final int FILAS = 4;
+    static final int COLS = 4;
 
-            /*  Proceso de la partida:
-            1. Cargar imágenes
-            2. Crear, barajar y desordenar matriz
-                https://es.stackoverflow.com/questions/296944/como-desordenar-mezclar-barajar-un-array-en-java
-            3. Iniciar contador de turnos, jugador inicial
-            4. Mostrar tablero
-            5. Bucle principal
-                6. Aceptar eventos: levantar dos cartas tapadas y comprobar si son iguales.
-                7. Comprobar si se han destapado todas las cartas.
-            8. Terminar Juego (Repetir?)
-            
+    static int[][] tableroOculto;
+    static int[][] tableroJugador = new int[FILAS][COLS];  // Almacena las casillas que gana cada jugador. 0 si aún
+                                                           // permanecen ocultas.
+    // Matriz para tablero en la interfaz gráfica
+    static ImageView[][] tablero = new ImageView[FILAS][COLS];
+    // Dimensiones de alto y ancho en píxeles de las casillas
+    final int ALTO_CASILLA = 200;
+    final int ANCHO_CASILLA = 200;
 
-            */
+    int turnoJugador = 1;
 
+    Image[] imagenes;
+    Image imgReverso;
+    static ImageView imageView1, imageView2;
+    GridPane gridPane;
+    private static Scene scene;
 
+    @Override
+    public void start(Stage stage) throws IOException {
 
+        tableroOculto = barajarTablero(8);
 
+        // Carga las imágenes
+        imagenes = new Image[8];
+        for (int i = 0; i < imagenes.length; i++) {
+            imagenes[i] = new Image(new FileInputStream("img/" + i + ".jpeg"));
+        };
+        imgReverso = new Image(new FileInputStream("img/cochesReverso.jpeg"));
 
-            //Creating an image 
-            Image[] imagenes = {
-                new Image(new FileInputStream("img/guerrero01.jpeg")),
-                new Image(new FileInputStream("img/elfo01.jpeg")),
-                new Image(new FileInputStream("img/enano01.jpeg")),
-                new Image(new FileInputStream("img/hobbit01.jpeg")),
-                new Image(new FileInputStream("img/orco01.jpeg")),
-                new Image(new FileInputStream("img/aranha01.jpeg")),
-                new Image(new FileInputStream("img/troll01.jpeg")),
-                new Image(new FileInputStream("img/dragon01.jpeg")),
-            };
-            
-            //Setting the image view 
-            //ImageView imageView = new ImageView(image);
+        gridPane = new GridPane();
 
-            GridPane gridPane = new GridPane();
-            for (int i = 0; i < FILAS; i++) {
-                for (int j = 0; j < COLS; j++) {
-                    System.out.println((i + j) % imagenes.length);
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                tablero[i][j] = new ImageView(imgReverso);
+                // setting the fit height and width of the image view
+                tablero[i][j].setFitHeight(ALTO_CASILLA);
+                tablero[i][j].setFitWidth(ANCHO_CASILLA);
+                // Setting the preserve ratio of the image view
+                tablero[i][j].setPreserveRatio(true);
 
-                    mapa[i][j] = new ImageView(imagenes[(i + j + 1) % imagenes.length]);
-                          //setting the fit height and width of the image view 
-                          mapa[i][j].setFitHeight(ALTO_CASILLA); 
-                          mapa[i][j].setFitWidth(ANCHO_CASILLA); 
-                          
-                          //Setting the preserve ratio of the image view 
-                          mapa[i][j].setPreserveRatio(true);    
+                tablero[i][j].addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
 
-                          EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
-
-                            @Override
-                            public void handle(MouseEvent arg0) {
-                                ImageView imgV = (ImageView) arg0.getSource();
-                                System.out.println(arg0.getSource());
-                                try {
-                                    imgV.setImage(new Image(new FileInputStream("img/enano02.jpeg")));
-                                } catch (FileNotFoundException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                }
-                                
-                            }
-
-                          };
-                          mapa[i][j].addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);                
-
-                    gridPane.add(mapa[i][j], i, j, 1, 1);
-                }
+                gridPane.add(tablero[i][j], i, j, 1, 1);
             }
-    
-            scene = new Scene(gridPane, 400, 400);
-            scene.setFill(Color.ROSYBROWN);
-            stage.setScene(scene);
-            stage.setTitle("Memory");
-            stage.show();        
+        }
+
+        //FlowPane pane = new FlowPane();
+
+        // Crea y configura la escena y muestra el escenario.
+        scene = new Scene(gridPane, COLS * ANCHO_CASILLA, FILAS * ALTO_CASILLA);
+        scene.setFill(Color.ROSYBROWN);
+        stage.setScene(scene);
+        stage.setTitle("Memory");
+        stage.show();
     }
 
+    private int[][] barajarTablero(int numImagenes) {
+        int filas = (int) Math.sqrt(numImagenes * 2);
+        int cols = (numImagenes * 2) / filas;
+        int[][] t = new int[filas][cols];
+        // Inicializar tablero
+        int indImg = 0;
+        for (int i = 0; i < t.length; i++)
+            for (int j = 0; j < t[0].length; j++) {
+                t[i][j] = indImg % numImagenes;
+                indImg++;
+            }
+
+        // Barajar (recorrer la matriz e intercambiar cada elemento con otro escogido
+        // aleatoriamente)
+        for (int i = 0; i < t.length; i++)
+            for (int j = 0; j < t[0].length; j++) {
+                int f = (int) (Math.random() * t.length);
+                int c = (int) (Math.random() * t.length);
+                int aux = t[i][j];
+                t[i][j] = t[f][c];
+                t[f][c] = aux;
+            }
+        return t;
+    }
+
+
+    EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent arg0) {
+            ImageView imgV = (ImageView) arg0.getSource();
+            int r = GridPane.getRowIndex(imgV);
+            int c = GridPane.getColumnIndex(imgV);
+
+            Image img = imgV.getImage();
+
+            // Si ha marcado la segunda casilla
+            if (imageView1 != null && imageView2 != null) {
+                int r1 = GridPane.getRowIndex(imageView1);
+                int c1 = GridPane.getColumnIndex(imageView1);
+                
+                // Si no son pareja les da la vuelta de nuevo
+                if (tableroJugador[r1][c1] == 0) {
+                    imageView1.setImage(imgReverso);
+                    imageView2.setImage(imgReverso);
+                }
+                imageView1 = null;
+                imageView2 = null;  
+            }
+
+            // Si destapa una nueva casilla
+            else if (img == imgReverso) {
+                // Destapa la casilla
+                imgV.setImage(imagenes[tableroOculto[r][c]]);
+                if (imageView1 == null)
+                    imageView1 = imgV;
+                else {
+                    if (imageView2 == null) {
+                        imageView2 = imgV;
+                        if (imageView2.getImage() == imageView1.getImage()) {
+                            // Pareja!!
+                            System.out.println("Pareja!!");
+                            r = GridPane.getRowIndex(imageView1);
+                            c = GridPane.getColumnIndex(imageView1);
+                            tableroJugador[r][c] = turnoJugador;
+                            r = GridPane.getRowIndex(imageView2);
+                            c = GridPane.getColumnIndex(imageView2);
+                            tableroJugador[r][c] = turnoJugador;
+
+                        } else {
+                            // No pareja => Volver a tapar
+                            System.out.println("No pareja!!");
+                            // Se tapan al siguiente click
+
+                        }
+                    }
+                }
+
+            }
+            /*
+             * else
+             * imgV.setImage(imgReverso)
+             */;
+        }
+    };
 
 
     public static void main(String[] args) throws Exception {
