fb9c16ea0c3d43c2e8b6fa350c41368f06e1017f
Óscar <aguado@edu.xunta.es>
2023-03-17 00:27:57 +0100

    JavaFX

--- UD07/Ejemplos JavaFX/src/hellofx/Main.java
+++ UD07/Ejemplos JavaFX/src/hellofx/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("rolfx.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("Personaje.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
