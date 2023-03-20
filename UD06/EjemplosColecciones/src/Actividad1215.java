7e8cedc8d1c8f84089635c762dd1d1f6644019d6
aguadodev <aguado@edu.xunta.es>
2023-03-20 13:30:56 +0100

    Mapas2

--- UD06/EjemplosColecciones/src/Actividad1215.java
+++ UD06/EjemplosColecciones/src/Actividad1215.java
@@ -1,5 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Actividad1215 {
     public static void main(String[] args) {
-        
+        Map<String, Integer> productos = new HashMap<>();
+
+        productos.put("Neumáticos", 20);
+        productos.put("Neumáticos", 20);
+        productos.put("Neumáticos", 20);
+
+
+        int opcion = leerMenu();
+        while (opcion != 0){
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            opcion = leerMenu();
+        }
+
+    }
+
+    static int leerMenu(){
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menú");
+        System.out.println("1. Alta");
+        System.out.println("2. Baja");
+        System.out.println("3. Actualiza existencias");
+        System.out.println("0. SALIR");
+        opcion = sc.nextInt();
+        return opcion;
     }
 }
