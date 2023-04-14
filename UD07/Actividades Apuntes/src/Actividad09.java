62a9acbe165c5fef5ecb82e780b82fb1adb17ea0
aguadodev <aguado@edu.xunta.es>
2023-04-14 13:20:55 +0200

    eventos, manejadores y filtros

--- UD07/Actividades Apuntes/src/Actividad09.java
+++ UD07/Actividades Apuntes/src/Actividad09.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,14 +11,20 @@ public class Actividad09 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Tablero");
+        
+        ImageView img1 = new ImageView(new Image("img/1-2.jpeg"));
+        img1.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println("Has hecho click en la imagen 1-2"));
 
-        Image i = new Image("img/1-2.jpeg");
-        i.
-        ImageView img = new ImageView(new Image("img/1-2.jpeg"));
+        ImageView img2 = new ImageView(new Image("img/3-4.jpeg"));
+        img1.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println("Has hecho click en la imagen 3-4"));
 
-        VBox vBox = new VBox(img);
+        //img1.setOnMouseClicked(e -> System.out.println("Has hecho click en la imagen 1-2"));
+        VBox vBox = new VBox(img1, img2);
+        vBox.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println("Has hecho click en la VBox"));
 
         Scene scene = new Scene(vBox);
+        scene.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println("Has hecho click en la escena"));
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
