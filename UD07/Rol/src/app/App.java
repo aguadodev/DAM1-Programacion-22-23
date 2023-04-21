a4ec32c510e7da4defa3f3aff2f25b8adb5e1778
aguadodev <aguado@edu.xunta.es>
2023-04-21 14:27:08 +0200

    ROL

--- UD07/Rol/src/app/App.java
+++ UD07/Rol/src/app/App.java
@@ -1,42 +1,25 @@
 package app;
-<<<<<<< HEAD
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-=======
-import java.io.IOException;
->>>>>>> 1facfbc70ec1511d3f4e97a0300976f91dfbd04c
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-<<<<<<< HEAD
 
 public class App extends Application{
 
     static Scene scene;
-=======
-import javafx.scene.Node;
-
-public class App extends Application{
-
-    private static Scene scene;
->>>>>>> 1facfbc70ec1511d3f4e97a0300976f91dfbd04c
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Juego de Rol");
-<<<<<<< HEAD
                 
         Parent root = FXMLLoader.load(getClass().getResource("/view/Inicio.fxml"));
 
         scene = new Scene(root);
-=======
-
-        scene = new Scene(new FXMLLoader(App.class.getResource("../view/Inicio.fxml")).load());
->>>>>>> 1facfbc70ec1511d3f4e97a0300976f91dfbd04c
         primaryStage.setScene(scene);
         primaryStage.show();
     }
