e80e0a7ab85af1b12071ef4da8fd2d992ab4c819
Aguado <aguado@edu.xunta.es>
2023-04-24 13:44:06 +0200

    EmpresaCRUD

--- UD07/Empresas/src/controller/EmpresaController.java
+++ UD07/Empresas/src/controller/EmpresaController.java
@@ -7,7 +7,7 @@ import app.App;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
 import model.Empresa;
 
 /**
@@ -17,6 +17,9 @@ public class EmpresaController implements Initializable{
 
     @FXML
     ListView<Empresa> empresasListView;
+
+    @FXML
+    TextField idTextField, nombreTextField, webTextField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -30,20 +33,36 @@ public class EmpresaController implements Initializable{
     }
 
     @FXML
-    void empresasMouseClicked(MouseEvent e){
-        System.out.println(e);
-        ListView empresas = (ListView) e.getSource();
-        // TODO...
+    void empresasMouseClicked(){
+        int i = empresasListView.getSelectionModel().getSelectedIndex();
+
+        idTextField.setText(String.valueOf(App.empresas.get(i).getId()));
+        nombreTextField.setText(App.empresas.get(i).getNombre());
+        webTextField.setText(App.empresas.get(i).getWeb());
+
     }
 
     @FXML
     void agregar(){
-        
+        Empresa empresa = new Empresa(Integer.parseInt(idTextField.getText()),
+                                    nombreTextField.getText(),
+                                    webTextField.getText());
+        App.empresas.add(empresa);
+        empresasListView.getItems().add(empresa);
     }
 
     @FXML
     void actualizar(){
-        
+        Empresa empresa = new Empresa(Integer.parseInt(idTextField.getText()),
+        nombreTextField.getText(),
+        webTextField.getText());
+
+        if (App.empresas.contains(empresa)){
+
+        } else {
+
+        }
+
     }
 
     @FXML
